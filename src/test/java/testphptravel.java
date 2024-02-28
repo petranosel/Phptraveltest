eadea8a800cbe908cf54c4238b395f21fedd202b
Petranosel <barnnabass@gmail.com>
2024-02-28 11:43:36 -0500

    my commit

--- src/test/java/testphptravel.java
+++ src/test/java/testphptravel.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class testphptravel {
+
+
+
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class testphptravel {
+
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void openPhpTravelsDemo() {
+
+        driver.get("https://phptravels.com/demo/");
+
+
+        //perform assertions here
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        // Close the browser
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
