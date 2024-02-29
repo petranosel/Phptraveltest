a3295d36707679837dde84c90e1c8e08943b85b4
petranosel <barnnabass@gmail.com>
2024-02-29 00:25:46 -0500

    Update testphptravel.java

--- src/test/java/testphptravel.java
+++ src/test/java/testphptravel.java
@@ -34,48 +34,7 @@ public class testphptravel {
 
         //perform assertions here
     }
-    @Test
-    public void testRegistration() {
-        // Locate elements by their IDs, names, or any other suitable locators
-        WebElement firstNameInput = driver.findElement(By.name("firstname"));
-        WebElement lastNameInput = driver.findElement(By.name("lastname"));
-        WebElement emailInput = driver.findElement(By.name("email"));
-        WebElement passwordInput = driver.findElement(By.name("password"));
-        WebElement confirmPasswordInput = driver.findElement(By.name("confirmpassword"));
-        WebElement cityInput = driver.findElement(By.name("city"));
-        WebElement stateInput = driver.findElement(By.name("state"));
-        WebElement phoneInput = driver.findElement(By.name("phone"));
-        WebElement postcodeInput = driver.findElement(By.name("postcode"));
-        WebElement billingAddressInput = driver.findElement(By.name("billingaddress"));
-        WebElement additionalInfoInput = driver.findElement(By.name("additionalinfo"));
-        WebElement securityQuestionInput = driver.findElement(By.name("securityquestion"));
-        WebElement securityAnswerInput = driver.findElement(By.name("securityanswer"));
-        WebElement mailingListCheckbox = driver.findElement(By.name("mailinglist"));
-        WebElement registerButton = driver.findElement(By.xpath("//button[@type='submit']"));
-
-        // Input test data
-        firstNameInput.sendKeys("john");
-        lastNameInput.sendKeys("Doe");
-        emailInput.sendKeys("johndoe@example.com");
-        passwordInput.sendKeys("testpassword");
-        confirmPasswordInput.sendKeys("testpassword");
-        cityInput.sendKeys("City");
-        stateInput.sendKeys("State");
-        phoneInput.sendKeys("1234567890");
-        postcodeInput.sendKeys("12345");
-        billingAddressInput.sendKeys("123 Main St, City, Country");
-        additionalInfoInput.sendKeys("Additional information");
-        securityQuestionInput.sendKeys("What is your favorite color?");
-        securityAnswerInput.sendKeys("Blue");
-        mailingListCheckbox.click();
-
-        // Click the register button
-        registerButton.click();
-
-        // Wait for registration to complete and assert a success message or any other validation
-        WebElement successMessage = driver.findElement(By.xpath("//div[contains(@class, 'alert-success')]"));
-        assertTrue(successMessage.isDisplayed());
-    }
+    
     @AfterMethod
     public void tearDown() {
         // Close the browser
